a9e1c4816ade4b9cbcdf4f363cba200319086a96
BhanuBikki <bikki.usapp@gmail.com>
2016-07-13 17:40:39 -0400

    new updates

--- project1/README.md
+++ project1/README.md
@@ -2,3 +2,7 @@
 
 microblog
 work on progress.....
+
+
+
+Reference: http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- project1/app/views.py
+++ project1/app/views.py
@@ -1,11 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-	user = {'name' : 'bhanu'}
+	user = {'name' : 'bikki'}
 	teams = [
 		{
 			'person' : {'name' : 'bhanu'},
@@ -20,3 +21,14 @@ def index():
 							title = 'My page',
 							user = user,
 							teams = teams)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login request for OpenID = "%s", remember_me=%s' %(form.openid.data,str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html', 
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
