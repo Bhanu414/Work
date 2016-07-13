b52a12eff50a1b8a3c602765373983bcb9054363
BhanuBikki <bikki.usapp@gmail.com>
2016-07-13 00:42:59 -0400

    template

--- project1/app/views.py
+++ project1/app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-	return "Hello bhanu"
+	user = {'name' : 'bhanu'}
+	teams = [
+		{
+			'person' : {'name' : 'bhanu'},
+			'age' : '20'
+		},
+		{
+			'person' : {'name' : 'prakash'},
+			'age' : '23'
+		}
+	]
+	return render_template('index.html',
+							title = 'My page',
+							user = user,
+							teams = teams)
