ddd3f5cee8bd1a9d4657ba6fb691b24c49b0dff5
BhanuBikki <bikki.usapp@gmail.com>
2016-07-18 01:07:23 -0400

    undone

--- project1/app/views.py
+++ project1/app/views.py
@@ -1,6 +1,12 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, sessions,url_for, request, g
+from flask_login import login_user,logout_user,current_user,login_required
+from app import app,db,lm,oid
 from .forms import LoginForm
+from .models import User
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 
@@ -23,7 +29,10 @@ def index():
 							teams = teams)
 
 @app.route('/login',methods=['GET','POST'])
+@oid.loginhandler
 def login():
+	if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login request for OpenID = "%s", remember_me=%s' %(form.openid.data,str(form.remember_me.data)))
