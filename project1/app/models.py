23f12dd43f5b77bcb1f229113af8d268862a5722
BhanuBikki <bikki.usapp@gmail.com>
2016-07-26 18:32:09 -0400

    profile page and avatars

--- project1/app/models.py
+++ project1/app/models.py
@@ -1,11 +1,13 @@
 from app import db
-
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
 
     @property
     def is_authenticated(self):
@@ -28,6 +30,8 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
+    def avatar (self,size):
+        return 'https://s.gravatar.com/avatar/36811e9f80f570e2f9c5778dffb5438c?s=80%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(),size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
